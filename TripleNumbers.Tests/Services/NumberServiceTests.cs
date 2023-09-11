eaf783b5b102eed467fa81d22a5549f8b0a1627b
Maikel Bos <maikel.bos0@gmail.com>
2023-09-11 19:49:29 +0200

    Add service to calculate triplets with tests

---
+++ TripleNumbers.Tests/Services/NumberServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using TripleNumbers.Services;
+
+namespace TripleNumbers.Tests.Services {
+    public class NumberServiceTests {
+        [Theory]
+        [InlineData("", false, null)]
+        [InlineData("[]", false, null)]
+        [InlineData("[112, he3,2,1,9,9,8]", false, null)]
+        [InlineData("[1,2,3,4,5,1,1]", true, "[1]")]
+        [InlineData("[9,3,3,2,1,1,1,2,9,9,9]", true, "[9,1]")]
+        [InlineData("[1,1,1,1,3,3,3,3,2,1,2,9,9,9]", true, "[9,3,1]")]
+        [InlineData("[1,2,3,2,1,9,9,8]", true, "[]")]
+        public void TryGetTripleIntegers(string input, bool expectedIsValid, string? expectedOutput) {
+            var subject = new NumberService();
+
+            Assert.Equal(expectedIsValid, subject.TryGetTripleIntegers(input, out var output));
+            Assert.Equal(expectedOutput, output);
+        }
+    }
+}
---
+++ TripleNumbers/Services/NumberService.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace TripleNumbers.Services {
+    public class NumberService {
+        public const string expectedInputFormat = "^\\[-?\\d+(,-?\\d+)*\\]$";
+
+        private readonly static Regex inputValidator = new(expectedInputFormat, RegexOptions.Compiled);
+
+        public bool TryGetTripleIntegers(string input, [NotNullWhen(true)] out string? output) {
+            output = null;
+
+            if (!inputValidator.IsMatch(input)) {                
+                return false;
+            }
+
+            try {
+                var integers = input[1..^1].Split(',').Select(int.Parse);                
+                var triples = integers
+                    .GroupBy(i => i)
+                    .Where(i => i.Count() >= 3)
+                    .Select(i => i.Key)
+                    .OrderByDescending(i => i);
+
+                output = $"[{string.Join(",", triples)}]";
+                return true;
+            }
+            catch {
+                return false;
+            }
+        }
+    }
+}
