9eeb7711fe754e4ab63ba8c0b30dbdcdfde2c7a4
Maikel Bos <maikel.bos0@gmail.com>
2023-09-11 19:32:22 +0200

    Add regular expression validation

--- TripleNumbers/Models/HomeModel.cs
+++ TripleNumbers/Models/HomeModel.cs
@@ -3,6 +3,7 @@
 namespace TripleNumbers.Models {
     public class HomeModel {
         [Required(ErrorMessage = "Please provide a sequence of numbers")]
+        [RegularExpression("^\\[-?\\d+(,-?\\d+)*\\]$", ErrorMessage = "Please provide a sequence of numbers in the format [1,3,2,3,4,3,5]")]
         public string? Input { get; set; }
         public string? Output { get; set; }
     }
